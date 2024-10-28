4a0ddb239d95115c22601f3bceabed4c17a2614e
Salman <s.z.gassem@gmail.com>
2024-10-28 21:45:27 +0300

    fixing logo

--- app/Home.py
+++ app/Home.py
@@ -21,8 +21,8 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 with st.sidebar:
-      add_logo("app/logo4.png")
-      st.caption("By Salman Gassem © 2024")
+    add_logo("app/logo4.png")
+    st.caption("By Salman Gassem © 2024")
 
 st.title(":eye: Eyedentify")
 
